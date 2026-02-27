c37bab69f9d03cbd54d8fd215df5c55dddf86eb4
rohitvishin <rohitvishwakarma65@gmail.com>
2026-02-27 15:18:57 +0530

    commented unused

--- server/index.js
+++ server/index.js
@@ -6,35 +6,35 @@ const helmet = require('helmet');
 
 const keys = require('./config/keys');
 const routes = require('./routes');
+const socket = require('./socket');
 const setupDB = require('./utils/db');
 
+const { port } = keys;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(
   helmet({
     contentSecurityPolicy: false,
     frameguard: true
   })
 );
+app.use(cors({
+  origin: "https://ecommerce-mern-37yt.vercel.app/",
+  credentials: true
+}));
 
-app.use(
-  cors({
-    origin: "https://ecommerce-mern-37yt.vercel.app",
-    credentials: true
-  })
-);
-
-// Connect DB
 setupDB();
-
-// Passport
 require('./config/passport')(app);
-
-// Routes
 app.use(routes);
 
-// ✅ IMPORTANT: Export app for Vercel
-module.exports = app;
+// const server = app.listen(port, () => {
+//   console.log(
+//     `${chalk.green('✓')} ${chalk.blue(
+//       `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+//     )}`
+//   );
+// });
+
+// socket(server);
