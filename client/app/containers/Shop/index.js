89673fa2f83e4f3766a16dbd656defeb074e96e3
rohitvishin <rohitvishwakarma65@gmail.com>
2026-03-13 17:10:44 +0530

    removed product filter

--- client/app/containers/Shop/index.js
+++ client/app/containers/Shop/index.js
@@ -48,7 +48,7 @@ class Shop extends React.PureComponent {
             md={{ size: 12, order: 1 }}
             lg={{ size: 3, order: 1 }}
           >
-            <ProductFilter filterProducts={filterProducts} />
+            <></>
           </Col>
           <Col
             xs={{ size: 12, order: 2 }}
